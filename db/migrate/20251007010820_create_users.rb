88a99ede43eaaa889ae9865f1532af84a8e80a4d
kitamurasaya141011 <saya.kitamura@carrot.co.jp>
2025-10-07 06:15:57 +0000

    テストに失敗しているようなので空行を削除

--- db/migrate/20251007010820_create_users.rb
+++ db/migrate/20251007010820_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-
       t.string :last_name
       t.string :first_name
       t.string :last_name_kana
