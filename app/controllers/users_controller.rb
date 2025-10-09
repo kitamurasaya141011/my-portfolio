f4d88682cdd2a3e5b9655b051d83863e7455bd1e
kitamurasaya141011 <saya.kitamura@carrot.co.jp>
2025-10-09 03:46:15 +0000

    コントロール内にユーザー登録メソッドを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
   def new
+
   end
 
+  # 登録画面の作成
   def create
+    user = User.new(user_params)
+    user.save!
+    redirect_to users_url, notice: "タスク 「#{user.name}」を登録しました。 "
   end
 
   def show
