d7055b3268c41abe17b834853f93ca223c7598fe
kitamurasaya141011 <saya.kitamura@carrot.co.jp>
2025-10-16 08:28:44 +0000

    一覧表示に必要なfileとコードを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,20 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :student, allow_destroy: true
   accepts_nested_attributes_for :teacher, allow_destroy: true
 
+  # 定数として性別のマッピングを定義
+  GENDERS = { 0 => '未設定', 1 => '男性', 2 => '女性', 3 => 'その他' }.freeze
+
+  # インスタンスメソッドとして表示名を取得する
+  def gender_label
+    GENDERS.fetch(self.gender.to_i, '不明')
+  end
+
   # roleを使用する方法がうまくいかなかったのでフォームで種別を保持するための仮想属性を作る
   attr_accessor :user_type
 
   # 学生の必須フィールド（例: student_id）に条件付きバリデーションを適用
   validates :student_id, presence: true, if: :student_selected?
-  # 👆 これが「required: true」の代わりとして機能します。
+  # これが「required: true」の代わりとして機能します。
 
   # フォームで送信された値に基づいて学生が選択されているかを判定するメソッド
   def student_selected?
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'users#index'
-  resources :users, only: [:index, :create, :new, :destroy]
+  resources :users, only: [:index, :create, :new, :destroy, :show]
   # get "users/new"
   # get "users/create"
   # get "users/show"
